81f344a10fe158269b58e8a118436106dfaef499
DanieleBaiocco <daniele.baiocco@studenti.unicam.it>
2021-11-23 13:15:34 +0100

    erased the unused code and generated the jar file and the script to execute it

---
+++ Tablut/src/it/unibo/ai/didattica/competition/ai/geneticAlgorithm/Agent.java
@@ -0,0 +1,26 @@
+package it.unibo.ai.didattica.competition.ai.geneticAlgorithm;
+
+import java.util.List;
+
+public class Agent {
+    public void setGenes(List<Double> asList) {
+    }
+
+    public void setScore(double v) {
+    }
+
+    public boolean isUndefined() {
+        return false;
+    }
+
+    public int getScore() {
+        return 0;
+    }
+
+    public Double getGene(int i) {
+        return 0.0;
+    }
+
+    public void setGene(int gene_number, double v) {
+    }
+}
---
+++ Tablut/src/it/unibo/ai/didattica/competition/ai/geneticAlgorithm/Population.java
@@ -0,0 +1,24 @@
+package it.unibo.ai.didattica.competition.ai.geneticAlgorithm;
+
+public class Population {
+    public int getSize() {
+        return 0;
+    }
+
+    public Agent getIndividual(int i) {
+        return new Agent();
+    }
+
+    public void add(Agent individual) {
+    }
+
+    public void SortPopulation() {
+    }
+
+    public void setIndividual(int i, Agent individual) {
+    }
+
+    public Agent[] getPopulation() {
+        return null;
+    }
+}
